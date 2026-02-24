b1270b6f02c6f8c197071ce00e97fb16c9d117f0
mathias <mathias.meeus@student.kdg.be>
2026-02-24 12:22:06 +0100

    feat: add audience validation to token parsing in auth.go

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -94,6 +94,7 @@ func extractToken(request *http.Request) (string, error) {
 func validateToken(tokenString string, jwks *keyfunc.JWKS, issuer, audience string) (jwt.MapClaims, error) {
 	token, err := jwt.Parse(tokenString, jwks.Keyfunc,
 		jwt.WithIssuer(issuer),
+		jwt.WithAudience(audience),
 		jwt.WithValidMethods([]string{"RS256"}),
 	)
 
@@ -107,11 +108,6 @@ func validateToken(tokenString string, jwks *keyfunc.JWKS, issuer, audience stri
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
-		return nil, ClaimsInvalid
-	}
-
-	azp, ok := claims["azp"].(string)
-	if !ok || azp != audience {
 		return nil, ClaimsInvalid
 	}
 
