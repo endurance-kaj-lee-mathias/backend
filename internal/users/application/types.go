6b084f41d5f514e6268e0b4fab7d11a15bf36bfe
mathias <mathias.meeus@student.kdg.be>
2026-03-04 21:27:50 +0100

    feat: add endpoints and service methods for updating first and last name
    - Added `PatchFirstName` and `PatchLastName` handlers.
    - Introduced corresponding service methods `UpdateFirstName` and `UpdateLastName`.
    - Extended router to include new `/me/first-name` and `/me/last-name` endpoints.

--- internal/users/application/types.go
+++ internal/users/application/types.go
@@ -16,6 +16,8 @@ type Service interface {
 	GetByUsername(ctx context.Context, username string) (domain.User, error)
 	DeleteUser(ctx context.Context, id domain.UserId) error
 	UpdatePhoneNumber(ctx context.Context, id domain.UserId, phoneNumber *string) error
+	UpdateFirstName(ctx context.Context, id domain.UserId, firstName string) error
+	UpdateLastName(ctx context.Context, id domain.UserId, lastName string) error
 	UpdateIntroduction(ctx context.Context, id domain.UserId, introduction string) error
 	UpdateAbout(ctx context.Context, id domain.UserId, about string) error
 	UpdateImage(ctx context.Context, id domain.UserId, image string) error
